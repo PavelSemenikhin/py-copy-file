2716652cca0ab70f8a753581ea3713d314e48734
Pavlo Semenikhin <pashock1337@gmail.com>
2025-07-08 23:01:31 +0300

    solution py copy file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(command: str) -> None:
+    split_command = command.split(" ")
+    if len(split_command) == 3 and split_command[0] == "cp":
+        source_file = split_command[1]
+        destination_file = split_command[2]
+
+        if source_file == destination_file:
+            return
+
+        try:
+            with (open(source_file, "r") as file,
+                  open(destination_file, "w") as new_file):
+                new_file.write(file.read())
+        except FileNotFoundError:
+            print(f"Source file '{source_file}' not found.")
